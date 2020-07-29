7e512092fc4e5b6b2b0e3d823c0d2b4766da6789
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-07-29 16:35:13 -0400

    added POI view

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Kalvin Zhao on 7/29/20.
+//  Copyright © 2020 KalvinZhao. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
